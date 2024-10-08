58e9879558e3a6e5e5952761c8e6301bd8a81b0f
daeunleeeee <lde7953@gmail.com>
2024-10-09 02:42:37 +0900

    feat(DF-14): 바텀시트 컴포넌트 추상화, 메인에 추가 #in-progress

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,13 +1,25 @@
 import Layout from '../../styles/BottomBarLayout'
 import { KakaoMap } from '../../components/Map'
-import React from 'react'
+import React, { useState } from 'react'
+import { BottomSheet } from '../../components/bottomsheet/BottomSheet'
 
-function index() {
+function Main() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <Layout>
       <KakaoMap />
+      <BottomSheet 
+        isOpen={isOpen} 
+        height="85vh" 
+        initialHeight="25vh" 
+        onClose={() => setIsOpen(false)}
+        onOpen={() => setIsOpen(true)}
+      >
+        <h3>바텀 시트 기본 골격</h3>
+      </BottomSheet>
     </Layout>     
   )
 }
 
-export default index
+export default Main
