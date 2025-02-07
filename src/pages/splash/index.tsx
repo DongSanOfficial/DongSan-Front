e8d2834a83980eef64bb40983450d112a8f8fef9
daeunleeeee <lde7953@gmail.com>
2025-02-07 15:34:27 +0900

    hotfix: 리프레시 관련 수정 코드 되돌림

--- src/pages/splash/index.tsx
+++ src/pages/splash/index.tsx
@@ -1,56 +1,84 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { getCookie } from "src/utils/cookieUtils";
-import { useLocationStore } from "../../store/useLocationStore";
-import { LocationState } from "../../store/locationStore.type";
-import instance from "../../apis/instance";
-import SplashScreen from "src/components/SplashScreen";
-import { refreshTokens } from "src/apis/auth";
+import axios, { AxiosError, AxiosResponse } from "axios";
+import { getCookie, removeCookie } from "src/utils/cookieUtils";
+import { refreshTokens } from "./auth";
 
-export default function Splash() {
-  const navigate = useNavigate();
-  const getCurrentLocation = useLocationStore(
-    (state: LocationState) => state.getCurrentLocation
-  );
+const baseURL = process.env.REACT_APP_BASE_URL;
 
-  useEffect(() => {
-    const initializeApp = async () => {
-      try {
-        const location = await getCurrentLocation();
-        console.log("스플래시 화면에서 받아온 현재 위치:", location);
+export const instance = axios.create({
+  baseURL,
+  timeout: 5000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
+});
 
-        const accessToken = getCookie("accessToken");
-        const refreshToken = getCookie("refreshToken");
+// const handleLogout = async () => {
+//   try {
+//     // 로그아웃 API 호출
+//     await instance.delete("/auth/logout");
+//   } catch (error) {
+//     console.error("로그아웃 API 호출 실패:", error);
+//     // API 호출 실패 시에도 토큰 제거
+//     removeCookie("access_token");
+//     removeCookie("refresh_token");
+//   } finally {
+//     window.location.href = "/signin";
+//   }
+// };
 
-        if (!accessToken || !refreshToken) {
-          setTimeout(() => navigate("/signin"), 2000);
-          return;
-        }
-
-        const response = await instance.post("/dev/token/expired", {
-          accessToken,
-          refreshToken,
-        });
-
-        const { accessTokenExpired, refreshTokenExpired } = response.data.data;
-
-        if (!accessTokenExpired && !refreshTokenExpired) {
-          //나중에 여기 "/main" 으로 변경하기
-          setTimeout(() => navigate("/navigation"), 2000);
-        } else if (accessTokenExpired && !refreshTokenExpired) {
-          const success = await refreshTokens(refreshToken);
-          setTimeout(() => navigate(success ? "/main" : "/signin"), 2000);
-        } else {
-          setTimeout(() => navigate("/signin"), 2000);
-        }
-      } catch (error) {
-        console.error("초기화 중 오류 발생:", error);
-        setTimeout(() => navigate("/signin"), 2000);
-      }
-    };
-
-    initializeApp();
-  }, []);
-
-  return <SplashScreen />;
+// 요청 인터셉터
+instance.interceptors.request.use(
+  (config) => {
+    const accessToken = getCookie("accessToken");
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+interface RetryConfig {
+  _retry?: boolean;
 }
+// 응답 인터셉터
+instance.interceptors.response.use(
+  (response: AxiosResponse) => response,
+  async (error: AxiosError) => {
+    const originalRequest = error.config;
+    if (!originalRequest) return Promise.reject(error);
+
+    const config = originalRequest as RetryConfig & typeof originalRequest;
+
+    if (originalRequest.url === "/auth/refresh") {
+      // handleLogout();
+      return Promise.reject(error);
+    }
+    if (error.response?.status === 401 && !config._retry) {
+      config._retry = true;
+
+      try {
+        const refreshToken = getCookie("refreshToken");
+        console.log("리프레시 토큰: ", refreshToken);
+        if (!refreshToken) throw new Error("리프레시 토큰이 없음");
+
+        const success = await refreshTokens(refreshToken);
+        if (success) {
+          const newAccessToken = getCookie("accessToken");
+          config.headers.Authorization = `Bearer ${newAccessToken}`;
+          return instance(originalRequest);
+        }
+        throw new Error("토큰 리프레시 실패");
+      } catch (refreshError) {
+        console.error("토큰 리프레시 실패", refreshError);
+        // handleLogout();
+        return Promise.reject("로그아웃되었습니다. 다시 로그인 해주세요.");
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
