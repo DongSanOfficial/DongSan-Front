39902758c1369c91ea8ce86502cd1796d5e0979c
sungwonnoh <jessica20730@naver.com>
2025-02-15 00:00:14 +0900

    feat(DF-69):북마크에 산책로 저장 및 삭제 axios코드

---
+++ src/apis/bookmark.ts
@@ -0,0 +1,37 @@
+import { ApiResponseFormat } from "./api.type";
+import { addToBookmark } from "./bookmark.type";
+import instance from "./instance";
+
+export const SaveToBookmark = async ({
+  //북마크에 산책로를 추가 api
+  bookmarkId,
+}: {
+  bookmarkId: number;
+}): Promise<addToBookmark> => {
+  try {
+    const response = await instance.post<ApiResponseFormat<addToBookmark>>(
+      `/bookmarks/${bookmarkId}/walkways`
+    );
+    return response.data.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const RemoveToBookmark = async ({
+  //북마크에 산책로를 추가 api
+  bookmarkId,
+  walkwayId,
+}: {
+  bookmarkId: number;
+  walkwayId: number;
+}): Promise<{}> => {
+  try {
+    const response = await instance.delete<ApiResponseFormat<{}>>(
+      `/bookmarks/${bookmarkId}/walkways/${walkwayId}`
+    );
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
---
+++ src/apis/bookmark.type.ts
@@ -0,0 +1,4 @@
+//북마크에 산책로를 추가
+export interface addToBookmark {
+  walkwayId: number;
+}
