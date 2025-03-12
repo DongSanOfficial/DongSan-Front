c5dc2f34276531ee190a5dfe1d89077a4755e60b
daeunleeeee <lde7953@gmail.com>
2025-03-13 04:13:11 +0900

    hotfix: 위치 접근 허용 여부에 따른 네비게이션 처리는 후순위로 변경

--- src/pages/splash/index.tsx
+++ src/pages/splash/index.tsx
@@ -1,62 +1,38 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { useLocationStore } from "../../store/useLocationStore";
+import { LocationState } from "../../store/locationStore.type";
 import instance from "../../apis/instance";
 import SplashScreen from "src/components/SplashScreen";
-import { useLocationPermission } from "../../hooks/useLocationPermission";
 
 export default function Splash() {
   const navigate = useNavigate();
-  const [isLoading, setIsLoading] = useState(true);
-  const { permissionStatus, location } = useLocationPermission();
+  const getCurrentLocation = useLocationStore(
+    (state: LocationState) => state.getCurrentLocation
+  );
 
   useEffect(() => {
     const initializeApp = async () => {
-      if (permissionStatus === "checking") {
-        return;
-      }
-
-      if (permissionStatus !== "granted") {
-        console.log("위치 정보 권한이 필요합니다.");
-        setIsLoading(false);
-        navigate("/locationAcceptance");
-        return;
-      }
-
       try {
-        await proceedWithTokenCheck();
-      } catch (error) {
-        console.error("초기화 중 오류 발생:", error);
-        setTimeout(() => {
-          setIsLoading(false);
-          navigate("/signin");
-        }, 2000);
-      }
-    };
+        const location = await getCurrentLocation();
+        console.log("스플래시 화면에서 받아온 현재 위치:", location);
 
-    const proceedWithTokenCheck = async () => {
-      try {
         const response = await instance.post("/dev/token/expired");
         const { accessTokenExpired, refreshTokenExpired } = response.data;
 
-        setTimeout(() => {
-          setIsLoading(false);
-          if (!accessTokenExpired && !refreshTokenExpired) {
-            navigate("/main");
-          } else {
-            navigate("/signin");
-          }
-        }, 2000);
+        if (!accessTokenExpired && !refreshTokenExpired) {
+          setTimeout(() => navigate("/main"), 2000);
+        } else {
+          setTimeout(() => navigate("/signin"), 2000);
+        }
       } catch (error) {
-        console.error("토큰 확인 중 오류 발생:", error);
-        setTimeout(() => {
-          setIsLoading(false);
-          navigate("/signin");
-        }, 2000);
+        console.error("초기화 중 오류 발생:", error);
+        setTimeout(() => navigate("/signin"), 2000);
       }
     };
 
     initializeApp();
-  }, [navigate, permissionStatus, location]);
+  }, []);
 
-  return isLoading ? <SplashScreen /> : null;
+  return <SplashScreen />;
 }
