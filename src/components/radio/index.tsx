327c213db295b47eab12eab9ab5d17f6e1b9340e
daeunleeeee <lde7953@gmail.com>
2025-05-28 03:47:28 +0900

    feat(DF-114): 라디오 버튼 커스터마이징

--- src/components/radio/index.tsx
+++ src/components/radio/index.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { theme } from "src/styles/colors/theme";
 
 const Container = styled.div`
   display: flex;
@@ -19,6 +20,31 @@ const RadioLabel = styled.label`
   cursor: pointer;
 `;
 
+const HiddenRadio = styled.input.attrs({ type: "radio" })`
+  display: none;
+`;
+
+const CustomRadio = styled.span<{ checked: boolean }>`
+  width: 16px;
+  height: 16px;
+  border: 2px solid ${({ checked }) => (checked ? theme.Green500 : "#ccc")};
+  border-radius: 50%;
+  display: inline-block;
+  position: relative;
+
+  &::after {
+    content: "";
+    display: ${({ checked }) => (checked ? "block" : "none")};
+    width: 8px;
+    height: 8px;
+    background: ${theme.Green500};
+    border-radius: 50%;
+    position: absolute;
+    top: 2px;
+    left: 2px;
+  }
+`;
+
 interface Option {
   label: string;
   value: string;
@@ -34,18 +60,21 @@ export default function Radio({ options, selectedValue, onChange }: RadioProps) 
   return (
     <Container>
       <RadioWrapper>
-        {options.map((option) => (
-          <RadioLabel key={option.value}>
-            <input
-              type="radio"
-              name="radio-button"
-              value={option.value}
-              checked={selectedValue === option.value}
-              onChange={() => onChange(option.value)}
-            />
-            {option.label}
-          </RadioLabel>
-        ))}
+        {options.map((option) => {
+          const isChecked = selectedValue === option.value;
+          return (
+            <RadioLabel key={option.value}>
+              <HiddenRadio
+                name="radio-button"
+                value={option.value}
+                checked={isChecked}
+                onChange={() => onChange(option.value)}
+              />
+              <CustomRadio checked={isChecked} />
+              {option.label}
+            </RadioLabel>
+          );
+        })}
       </RadioWrapper>
     </Container>
   );
