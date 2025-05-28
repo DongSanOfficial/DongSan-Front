0320ecd660d1ddd5c2d6bebecc524db8b99681c5
sungwonnoh <jessica20730@naver.com>
2025-05-28 15:10:47 +0900

    feat(DF-117): 크루 피드 ui개발

---
+++ src/pages/community/detail/components/FeedList.tsx
@@ -0,0 +1,43 @@
+import { theme } from "src/styles/colors/theme";
+import styled from "styled-components";
+
+const Container = styled.div`
+  border: 2px solid ${theme.Black};
+  border-radius: 8px;
+  width: 100%;
+  height: 5.5rem;
+  padding: 0.5rem;
+  box-shadow: 4px 4px rgba(0, 0, 0, 0.3);
+`;
+const Date = styled.div`
+  font-size: 14px;
+  font-weight: 500;
+  width: 100%;
+  display: flex;
+  justify-content: end;
+`;
+const Record = styled.div`
+  font-size: 18px;
+  height: 3rem;
+  font-weight: 600;
+  display: flex;
+  align-items: center;
+  margin: 0 0.5rem;
+`;
+const Bold = styled.span`
+  font-weight: 700;
+  font-size: 20px;
+  color: ${theme.Green500};
+  margin-left: 0.5rem;
+`;
+export default function FeedList() {
+  return (
+    <Container>
+      <Date>2025.05.25</Date>
+      <Record>
+        노성원님이
+        <Bold>1.5km</Bold> 를 산책하였습니다.
+      </Record>
+    </Container>
+  );
+}
--- src/pages/community/detail/feed/index.tsx
+++ src/pages/community/detail/feed/index.tsx
@@ -1,3 +1,36 @@
+import styled from "styled-components";
+import FeedList from "../components/FeedList";
+
+const Daysago = styled.div`
+  font-weight: 600;
+  font-size: 15px;
+  margin: 0.5rem 0.2rem;
+`;
+const ListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
 export default function Feed() {
-  return <div>피드 내용 여기에</div>;
+  // 날짜 차이를 구해서 "오늘", "어제", "n일 전"으로 변환하는 함수
+  const getDaysAgoLabel = (dateString: string): string => {
+    const today = new Date();
+    const targetDate = new Date(dateString);
+    const diffTime = today.getTime() - targetDate.getTime();
+    const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+
+    if (diffDays === 0) return "오늘";
+    if (diffDays === 1) return "어제";
+    return `${diffDays}일 전`;
+  };
+  return (
+    <div>
+      <Daysago>오늘</Daysago>
+      <ListContainer>
+        <FeedList />
+        <FeedList />
+      </ListContainer>
+    </div>
+  );
 }
