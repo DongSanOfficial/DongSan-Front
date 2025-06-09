4c3f07096b1c23d6845fb6dbb5f048b753cc71b4
daeunleeeee <lde7953@gmail.com>
2025-06-09 22:52:00 +0900

    feat(DF-124): 크루 생성 api 연동

--- src/apis/crew/crew.ts
+++ src/apis/crew/crew.ts
@@ -1,7 +1,8 @@
 import { AxiosError } from "axios";
 import { ApiErrorResponse } from "../api.type";
 import instance from "../instance";
-import { feedList } from "./crew.type";
+import { CheckCrewNameResponse, CreateCrewRequest, CreateCrewResponse, CrewListParams, CrewListResponse, feedList, UploadCrewImageResponse } from "./crew.type";
+
 
 export const getIsBookmarked = async ({
   crewId,
@@ -19,4 +20,30 @@ export const getIsBookmarked = async ({
       axiosError.response?.data?.message || "피드 조회에 실패했습니다."
     );
   }
+};
+
+// 이름 중복 체크 api
+export const checkCrewName = async (name: string): Promise<boolean> => {
+  const { data } = await instance.get<CheckCrewNameResponse>("/crews/exists", {
+    params: { name },
+  });
+  return data.isValid;
+};
+
+// 이미지 업로드
+export const uploadCrewImage = async (crewImage: File): Promise<UploadCrewImageResponse> => {
+  const formData = new FormData();
+  formData.append("crewImage", crewImage);
+
+  const { data } = await instance.post<UploadCrewImageResponse>("/crews/image", formData, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
+
+  return data;
+};
+
+// 크루 생성 api
+export const createCrew = async (body: CreateCrewRequest): Promise<number> => {
+  const { data } = await instance.post<CreateCrewResponse>("/crews", body);
+  return data.crewId;
 };
